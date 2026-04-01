d2afb01360e840adcee71913efba783d35def1f6
kushal009-arch <153494753+kushal009-arch@users.noreply.github.com>
2026-04-01 21:27:53 +1100

    Convert num strings to int

--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
 def compute(expression):
     num0, operator, num1 = expression.split(' ') # This is the "unpacking"
+    num0, num1 = int(num0), int(num1)
     if operator == '+':
         return num0 + num1
     elif operator == '-':
